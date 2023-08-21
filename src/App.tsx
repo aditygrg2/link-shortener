904e125c6ec111f9cbb2db212a2fcb2fba133b06
Prince Rastogi <106924260+pre-Coded@users.noreply.github.com>
2023-08-21 16:26:26 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react'
 import HomePage from './pages/HomePage'
-import { Provider } from 'react-redux';
+import { Provider, useSelector } from 'react-redux';
 import store from './store/store';
 import DrawerNav from './components/Navbar/DrawerNav';
 import { AnimatePresence } from 'framer-motion';
 import ParticlesContainer from './components/ParticlesContainer';
-import Login from './components/Login';
-import SignUp from './components/SignUp';
+import Register from './components/Register/Register';
 
-export default function App() {
+const App : React.FC = () => {
+
   return (
     <Provider store={store}>
       <AnimatePresence mode='wait'>
         <div className='h-screen w-screen overflow-hidden bg-black font-grotesque'>
           <DrawerNav />
-          <Login/>
-          <SignUp/>
+          <Register/>
           <HomePage />
         </div>
       </AnimatePresence>
     </Provider>
   )
 }
+
+export default App;
---
+++ src/components/Register/Register.tsx
@@ -0,0 +1,108 @@
+import React, { useEffect } from "react";
+
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
+import { RxArrowRight } from 'react-icons/rx';
+import { useSelector, useDispatch } from 'react-redux';
+import { toggleLogin } from "../../store/Slice/LoginSlice";
+import { BsGoogle } from "react-icons/bs";
+
+interface stateObj {
+    isLoginOpen: boolean,
+}
+
+const Register: React.FC = () => {
+    const isLoginOpen = useSelector((state: stateObj) => state.isLoginOpen);
+    const dispatch = useDispatch();
+
+    const [loginPage, setLoginPage] = useState(true);
+
+    
+    const [loading, setLoading] = useState(false);
+
+    const togglePage = () => {
+        setLoading(prev => !prev);
+
+        setTimeout(() => {
+            setLoading(prev => !prev);
+            setLoginPage(prev => !prev);
+        }, 400)
+
+    }
+
+
+    return (
+        <>
+            {isLoginOpen && <div onClick={() => dispatch(toggleLogin())} className={`h-full hidden lg:block w-full absolute z-20 backdrop-blur-xl`}></div>}
+            {
+                <div
+                    className={`h-full ${isLoginOpen ? 'lg:w-[40%] w-full opacity-100' : "w-0 opacity-0"} right-0 top-0 backdrop-filter backdrop-blur-md absolute overflow-hidden z-[100] transition-all duration-500 ${loginPage ? "background-login" : 'background-signup'}`}>
+
+
+                    <div className="absolute h-full w-full bg-gradient-to-b from-black/80 via-black/40 to-black/20">
+
+                        {
+                            <div className={`absolute h-full bg-gradient-to-r from-[#131E25] via-[#131E25]/50 to-[#131E25]/5 ${loading ? 'w-full' : 'w-0'} transition-all duration-400 top-0 left-0 z-[100] backdrop-blur-md`}></div>
+                        }
+
+                        <div className="w-full h-full py-4 px-2 relative overflow-x-hidden overflow-y-scroll text-white space-y-8 scrollbar-hidden">
+
+                            <div className="h-14 w-full">
+                                <button onClick={() => { dispatch(toggleLogin()); }} title="Go back" className="p-2 aspect-square rounded-full border border-gray-700 h-14 flex items-center justify-center cursor-pointer">
+                                    <RxArrowRight className="text-primaryButton font-bold text-2xl" />
+                                </button>
+                            </div>
+
+                            <h1 className="flex flex-col space-y-3 text-3xl text-gray-300 whitespace-normal md:whitespace-nowrap w-full p-2 text-center">
+                                <span className="text-4xl"> Welcome </span>
+
+                                <span>Let's Get you Started</span>
+
+                                <span className="text-primaryButton text-5xl italic">LogoName</span>
+                            </h1>
+
+                            <div className="flex items-center justify-center space-x-4">
+                                <button title="Sign up with Google" className="bg-primaryButton-0 bg-shadow p-4 rounded-md hover:bg-transparent transition-all duration-200">
+                                    <BsGoogle className="text-3xl" />
+                                </button>
+                            </div>
+
+                            <div className="flex items-center justify-center w-full space-x-4">
+                                <div className="h-[0.5px] w-36 bg-gray-400"></div>
+                                <h1 className="text-3xl">Or</h1>
+                                <div className="h-[0.5px] w-36 bg-gray-400"></div>
+                            </div>
+
+                            <form className="flex flex-col items-center space-y-6 relative">
+                                <div className="w-full flex flex-col space-y-2 items-center">
+                                    <input type='text' placeholder="Email" className='w-full h-14 lg:w-96 border border-gray-500 rounded-md  bg-transparent p-2 outline-none' />
+                                    <input type='password' placeholder="Password" className='w-full h-14 lg:w-96 border border-gray-500 rounded-md bg-transparent p-2 outline-none' />
+                                </div>
+
+                                <div className="flex items-center justify-between w-full lg:w-96 px-3">
+                                    {loginPage ? <button type="reset" className="text-gray-400 text-xs ">Forgot Password ?</button> : <div className="text-gray-400 text-xs space-x-1 flex items-center cursor-pointer">
+                                        <input type={'checkbox'} />
+                                        <button>Remember me</button>
+                                    </div>}
+
+                                    {
+                                        loginPage ?
+                                            <button onClick={togglePage} type="button" className="text-gray-400 text-xs ">New here? <span className="text-primaryButton opacity-80">Sign Up</span></button> :
+
+                                            <button onClick={togglePage} type="button" className="text-gray-400 text-xs ">Already Registerd? <span className="text-primaryButton opacity-80">Log In</span></button>
+                                    }
+
+                                </div>
+
+                                <input type='submit' value="Submit" className="border-[1px] border-primaryButton-0 bg-shadow  hover:bg-primaryButton-0 hover:text-white w-80 h-12 text-xl text-gray-300 rounded-md cursor-pointer tracking-wider" />
+                            </form>
+
+                        </div>
+                    </div>
+                </div>
+            }
+        </>
+    )
+}
+
+export default Register;
