dbbc796d107832ca23d1864007e4aaf9fc817b36
Aditya Garg <uber6707@gmail.com>
2023-08-22 20:39:26 +0530

    Update MainPage.tsx

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -2,13 +2,19 @@ import React from "react";
 import DrawerNav from "../components/Navbar/DrawerNav";
 import Register from "../components/Register/Register";
 import HomePage from "./HomePage";
+import NavBar from "../components/Navbar/NavBar";
 
 export default function MainPage() {
   return (
-    <div className="h-screen w-screen overflow-hidden bg-black font-grotesque">
-      <DrawerNav />
-      <Register />
-      <HomePage />
+    <div className="h-screen w-screen overflow-hidden font-grotesque background">
+      <div className="h-full w-full overflow-hidden bg-gradient-to-r from-black/80 via-black/60 to-black/0">
+        <NavBar />
+        <DrawerNav />
+        <div className="h-full w-full absolute top-0 overflow-hidden">
+          <Register />
+        </div>
+        <HomePage />
+      </div>
     </div>
   );
 }
